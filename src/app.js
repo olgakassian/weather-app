4da631a9dc3fd50f1686b46bd39e3fa5604c4e19
Olga Kassian <olgakassian@gmail.com>
2021-12-25 12:25:08 +0000

    Update the time

--- src/app.js
+++ src/app.js
@@ -1,7 +1,25 @@
-let apiKey = "57b2c40fdae71a6ba41d72685e3226e2";
-let city = "New York";
-let units = "metric";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day}, ${hours}:${minutes}`;
+}
 
 function displayTemperature(response) {
   let tempElement = response.data.main.temp;
@@ -14,7 +32,14 @@ function displayTemperature(response) {
   document.querySelector("#humidity").innerHTML = humidityElement;
   let windElement = response.data.wind.speed;
   document.querySelector("#wind").innerHTML = windElement;
+  let dateElement = formatDate(response.data.dt * 1000);
+  document.querySelector("#date").innerHTML = dateElement;
   console.log(response);
 }
+
+let apiKey = "57b2c40fdae71a6ba41d72685e3226e2";
+let city = "New York";
+let units = "metric";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
 
 axios.get(apiUrl).then(displayTemperature);
